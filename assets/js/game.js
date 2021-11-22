fc7b0162586785016cf394a48e0267f1e7e624b4
mlbarre <mlbarrera1538@gmail.com>
2021-11-21 20:43:59 -0600

    randomize order of ememy attacks

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,48 +30,54 @@ var fightOrSkip = function() {
 }
 
 var fight = function(enemy) {
+    var isPlayerTurn = true;
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
+    }
+
     while(playerInfo.health > 0 && enemy.health > 0) {
         if (fightOrSkip()); {
             break;
         }
         
     }
+    // SUBRACT THE VALUE OF -PLAYERATTACK- FROM THE VALUE OF -ENEMYHEALTH- AND USE THAT RESULT TO UPDATE THE VALUE IN THE -ENEMYHEALTH- VARIABLE
+    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+
+        enemy.health = Math.max(0, enemy.health - damage);
+        console.log (
+            playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
+            );
+
+        // CHECK ENEMY HEALTH
+        if (enemy.health <= 0) {
+            window.alert(enemy.name + " has died!");
+
+            playerInfo.money = playerInfo.money + 20;
+            break;
+            } else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            
+
+        // SUBRACT THE VALUE OF -ENEMYATTACK- FROM THE VALUE OF -PLAYERHEALTH- AND USE THAT RESULT TO UPDATE THE VALUE IN THE -PLAYERHEALTH- VARIABLE
+            } else {
+        var damage = randomNumber(enemy.attack - 3, enemy.attack);
+
+        playerInfo.health = Math.max(0, playerInfo.health - damage);
+            console.log(
+                enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
+                );
+
+        // CHECK PLAYER'S HEALTH
+        if (playerInfo.health <= 0) {
+            window.alert(playerInfo.name + " has died!");
+            break;
+            } else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+        };
+    }
+    isPlayerTurn = !isPlayerTurn;
 }
-
-// SUBRACT THE VALUE OF -PLAYERATTACK- FROM THE VALUE OF -ENEMYHEALTH- AND USE THAT RESULT TO UPDATE THE VALUE IN THE -ENEMYHEALTH- VARIABLE
-var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-
-enemy.health = Math.max(0, enemy.health - damage);
-console.log (
-    playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
-);
-
-// CHECK ENEMY HEALTH
-if (enemy.health <= 0) {
-window.alert(enemy.name + " has died!");
-
-playerInfo.money = Math.max(0, playerInfo.money - 10);
-
-    break;
-} else {
-window.alert(enemy.name + " still has " + enemy.health + " health left.");
-} 
-
-// SUBRACT THE VALUE OF -ENEMYATTACK- FROM THE VALUE OF -PLAYERHEALTH- AND USE THAT RESULT TO UPDATE THE VALUE IN THE -PLAYERHEALTH- VARIABLE
-var damage = randomNumber(enemy.attack - 3, enemy.attack);
-
-playerInfo.health = Math.max(0, playerInfo.health - damage);
-console.log(
-    enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
-);
-
-// CHECK PLAYER'S HEALTH
-if (playerInfo.health <= 0) {
-    window.alert(playerInfo.name + " has died!");
-    break;
-} else {
-    window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
-};
 
 var startGame = function() {
     //RESET PLAYER STATS
